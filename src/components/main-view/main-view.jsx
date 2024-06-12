a969bb0850ed363ede5d0e2efdd9138fcce05ca8
Jacob DiBiase <jakesback93@hotmail.com>
2024-06-11 23:15:39 -0700

    Import NavBar into main-view and add to BrowserRouter return

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import { NavigationBar } from "../navigation-bar/navigation-bar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
@@ -44,6 +45,12 @@ export const MainView = () => {
 
   return (
     <BrowserRouter>
+      <NavigationBar
+        user={user}
+        onLoggedOut={() => {
+          handleLogout();
+        }}
+      />
       <Row className="justify-content-md-center">
         <Routes>
           <Route
